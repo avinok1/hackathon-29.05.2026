9ee0d8b3d9413fd8452a09c2609c85e9f9aa8f1c
amglmerc <gi.amilka@bk.ru>
2026-05-31 14:12:52 +0300

    дбоавление логов в dist

--- src/distributor.py
+++ src/distributor.py
@@ -3,6 +3,8 @@
 from classifier import Classifier
 from dataclasses import dataclass
 from pathlib import Path
+import logging
+
 
 class Distributor:
     def distribute(self, emails, errors):
@@ -14,8 +16,11 @@ class Distributor:
             category_folder = result_way / category
             category_folder.mkdir(parents=True, exist_ok=True)
             file.rename(category_folder / file.name)
+            logging.info(f'файл {file.name} теперь в папке {category_folder}')
+    
 
         for file, error in errors:
             error_folder = result_way / 'errors'
             error_folder.mkdir(parents=True, exist_ok=True)
             file.rename(error_folder / file.name)
+            logging.error(f'файл {file.name} теперь в папке errors, причина = {error}')
